c9436052bf8906403f1a3905395776abe0501f26
Kemal Baran Topkaya <baran.topkaya@solidict.com>
2025-07-02 21:23:13 +0300

    cross fix

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Vite/Vue dev server
+                .allowedOrigins("http://localhost:5173",
+                        "https://imdb-frontend-chi.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
